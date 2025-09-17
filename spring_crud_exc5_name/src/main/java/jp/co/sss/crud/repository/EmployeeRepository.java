9000358ecb0c15be1b32727bbc1e0acb583c11c8
村瀬菜水香 <n_murase@braingate.onmicrosoft.com>
2025-09-17 10:50:55 +0900

    最初のコミット

---
+++ spring_crud_exc5_name/src/main/java/jp/co/sss/crud/repository/EmployeeRepository.java
@@ -0,0 +1,23 @@
+package jp.co.sss.crud.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.co.sss.crud.entity.Department;
+import jp.co.sss.crud.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+	
+	//ログイン
+	Employee findByempIdAndEmpPass(Integer empId, String empPass);
+	
+	//社員IDで全件検索
+	List<Employee> findAllByOrderByEmpIdAsc();
+	
+	//社員名検索
+	List<Employee> findByEmpNameContainingOrderByEmpId(String empName);
+	
+	//部署ID検索
+	List<Employee> findByDepartmentOrderByEmpId(Department department);
+}
